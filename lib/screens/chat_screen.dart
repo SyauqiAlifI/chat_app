440133a81c56fbb26027392d7288790202cc98e0
SyauqiAlifI <vstudio776@gmail.com>
2022-03-01 10:40:17 +0700

    five

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -24,7 +24,7 @@ class _ChatScreenState extends State<ChatScreen> {
     try {
       final user = _auth.currentUser;
       if(user != null) {
-        loggedInUser == user;
+        loggedInUser = user;
         print(loggedInUser.email);
       }
     } catch (e) {}
